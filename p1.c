d889e39a34ae9334b7dbe35206a106ad09f1616c
itsabhinavism <a.for.abhinav.2003@gmail.com>
2023-03-22 23:18:41 +0530

    Updated some codes

---
+++ p1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    float a = 0.4;
+    if (a == 0.4)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
---
+++ p2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   ;
+   int i;
+
+   a = printf("o\\n");
+   printf("%d\n", a);
+
+   for (printf("hello"), i = 5; i; scanf("%d", i++, &i))
+   {
+      printf("%d", i);
+   }
+
+   return 0;
+}
